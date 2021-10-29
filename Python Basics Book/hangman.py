24fd58a1f0e206c129c1ef2a9eff55ab602f21e8
Petahwil <Petahwil@gmail.com>
2021-10-29 11:28:58 +0000

    Clean Up Hangman

--- Python Basics Book/hangman.py
+++ Python Basics Book/hangman.py
@@ -1,98 +1,73 @@
-# def eletter():
-#     guess = input("\nEnter a letter or word:")
-#     guess = guess.upper()
-#     return guess;
 
-# def game(word, guess):
-#     guess_letter = guess
-#     result =False
-#     while rounds > 0:
-#         word = word
-#         failed = 0
-#         for char in word:
-#             if char in guess_letter:
-#                 print(char, end="")
-#             else:
-#                 print("_", end="")
-#                 failed += 1
-#         if (failed == 1) or (guess == word):
-#             print("\nYou Win!")
-#             result = True
-#             return result
-#         if len(guess) == 1:
-#             guess_letter += guess
-#             rounds -= 1
-#             if guess_letter.count(guess) > 1:
-#                 print("\nYou've already guessed that letter!")
-#             if guess not in word:
-#                 guess_letter = "".join(set(guess_letter))
-#                 print("\nYour guess so far: " + guess_letter)
-#             if rounds == 0:
-#                 print("\nYou lose! The word was " + word)
-#                 return result
-#     return result
-
-# def main():
-#     round = 6
-#     word_list = ['APPLE', 'OBVIOUS', 'XYLOPHONE']
-#     word_index = 0
-#     win = 0
-#     while word_index < 3:
-#         if game(word_list, word_index) is True:
-#             win += 1
-#         word_index += 1
-#     print("You won " + str(win) + " out of 3")
-
-# main()
-
-
-    
 def check_letter(word, guess, guess_letter, current_round):
     guessed_char = ""
+    #add round is set up to notify main if the user guesses wrong add a round to current round
     add_round = 0
+    # check if guess is in the word
     for char in word:
+        #looking through the guessed letters to put word together
         if char in guess_letter:
             print(char, end="")
+            # add the chars found to this string so we can use it through the funtion
             guessed_char += char
+        #if it is not found then put a blank in
         else:
             print("_", end="")
+    # If the guessed char makes up the word return 6 signal win
     if (guessed_char == word):
         print("\nYou Win! The word was " + word)
         return 6
+    #had to add minus roung becuase this runs ever time the next if runs and you dont want to add a round here
     if guess_letter.count(guess) > 1:
         add_round -= 1
         print("\nYou've already guessed that letter!")
+    #if they guess a letter wrong add a round and print out what they have guessed
     if guess not in word:
         add_round += 1
-        current_round += 1
         guess_letter = "".join(set(guess_letter))
         print("\nYour guess so far: " + guess_letter)
+    #if the user guesses wrong 6 times they lose return one to tell they
     if current_round == 6:
         print("\nYou lose! The word was " + word)
         return 1
+    # return round to add one if the user is wrong otherwise it will add 
     return add_round 
 
 
     
-def main2():
+def main():
     word_list = ['APPLE', 'BUTTHOLE', 'BEEFYBOI']
     total_round = 0
     add_guess = ''
+    #while there are words not used yet in the array keep playing game
     while total_round < len(word_list):
+        #resetting current round and the letters user guesses inbetween rounds
         current_round = 0
         guess_letter = ""
+        #loop for the game the user is currently playing
+        print(f'\nStart Hangman!')
         while current_round < 6:
+            #prompt the user for there guess
             guess = input("\nEnter a letter or word:").upper()
+            # if the guess is in the right format then continue the game if not have them re-enter
             if len(guess) == 1:
+                #add all the letters guessed to str
                 guess_letter += guess
-            did_you_win = check_letter(word_list[total_round] ,guess, guess_letter, current_round)
-            if did_you_win == 6:
+                #running funtion to check the letter that was guessed
+                did_you_win = check_letter(word_list[total_round] ,guess, guess_letter, current_round)
+                # return 6 user wins return 1 user guessed wrong add a round
+                if did_you_win == 6:
+                    current_round = 6
+                if did_you_win == 1:
+                    current_round += 1
+            # if user guesses word then they win otherwise entry invalid
+            if guess == word_list[total_round]:
+                print("\nYou Win! The word was " + word_list[total_round])
                 current_round = 6
-            if did_you_win == 1:
-                current_round += 1
+            else:
+                print(f'\nYou entry was invalid')
             print(f'\nYou have {6 - current_round} wrong guesses left')
             print(f'\n_____________________________')
-        # print(total_round)
         total_round +=1
 
-main2()
+main()
